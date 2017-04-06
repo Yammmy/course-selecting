ed269585b61769b05cf82466b79add9d7811bf3b
Yammy <yammyxing@foxmail.com>
2017-04-06 09:45:48 +0800

    connect student and course with n2n

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,7 @@
 class Student < ApplicationRecord
   has_many :teacher_relationships
   has_many :teachers, through: :teacher_relationships, source: :teacher
+
+  has_many :course_relationships
+  has_many :courses, through: :course_relationships, source: :course
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406013249) do
+ActiveRecord::Schema.define(version: 20170406013943) do
+
+  create_table "course_relationships", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
