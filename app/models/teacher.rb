37c97db31f8680aad10590df53200660443b13c4
Yammy <yammyxing@foxmail.com>
2017-04-06 09:48:29 +0800

    connect teacher and course with 1 to n

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,6 @@
 class Teacher < ApplicationRecord
   has_many :teacher_relationships
   has_many :students, through: :teacher_relationships, source: :student
+
+  has_many :courses, dependent: :destroy
 end
